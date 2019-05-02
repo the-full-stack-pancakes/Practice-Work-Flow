20aa499baf955f913a57052a4cf845fa4f992528
JeanAugustin <jre.augustin@gmail.com>
2019-05-02 13:00:42 -0400

    EVents and UI

--- assets/scripts/Books/ui.js
+++ assets/scripts/Books/ui.js
@@ -1,2 +1,8 @@
 
- const onGetBooksSuccess =
+ const onGetBooksSuccess = require('../templates/get-button.handlebars')
+
+ const getBooksSuccess = (data) => {
+   console.log(data)
+   const showBooksHtml = showBooksTemplate({ books: data.books })
+   $('.content').html(showBooksHtml)
+ }
